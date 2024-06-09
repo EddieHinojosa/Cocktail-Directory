f5e657463492f9b671d8442676e31e6b33299365
Zachary Polof <ZacharyPolof@gmail.com>
2024-06-09 16:54:51 -0500

    modified drink-details
    added form to surround input, capture user input for rating.

--- public/js/script.js
+++ public/js/script.js
@@ -5,3 +5,6 @@ startButton.addEventListener("click", (event) => {
   event.preventDefault();
   window.location.href = "/drink-search";
 });
+
+
+// capture form input from drink-details for ratings system.
