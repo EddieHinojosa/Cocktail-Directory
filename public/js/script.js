ebd8b2a0c27b79f726fe0c9a26a477b4d54acddf
Zachary Polof <ZacharyPolof@gmail.com>
2024-06-09 17:01:04 -0500

    Update script.js
    added client-side JS to capture form input post input and then log to console if it was accepted

--- public/js/script.js
+++ public/js/script.js
@@ -8,3 +8,27 @@ startButton.addEventListener("click", (event) => {
 
 
 // capture form input from drink-details for ratings system.
+document.getElementById("rating-form").addEventListener('submit', async function(event) {
+  event.preventDefault();
+
+    const form = event.target;
+    const formData = new FormData(form);
+    const ratingData = {
+        drink_id: formData.get('drink_id'),
+        rate: formData.get('rate')
+    };
+
+    const response = await fetch('/submit-rating', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(ratingData),
+    });
+
+    if (response.ok) {
+      alert('User Rating Accepted!');
+    } else {
+      alert('Failed to submit rating. Please try again.');
+    }
+});
